f7ca24976fe64aaa87ee2c1acf55b3582ad10d85
github username <48257792+ry-thornton@users.noreply.github.com>
2019-12-21 17:52:15 -0500

    home page

---
+++ flatiron-project-frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Home-page-greeting">
+        <span className="Logo">Flatiron Projects</span>
+        <br/>
+        <span>A platform for innovation</span>
+      </div>
+      <br/>
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default App;
